b482b09cf1c76d06f1df895aa99bc5f212ed2558
yukinoto <2733687567@qq.com>
2020-11-22 20:52:41 +0800

    .

--- code/p1429.cpp
+++ code/p1429.cpp
@@ -1,19 +1,42 @@
 #include <cstdio>
-#include <cmath>
-#include <algorithm>
-using namespace std;
 
+float sqrt(float x)
+{
+
+    float xhalf = 0.5f*x;
+    int i = *(int*)&x; 
+    
+    if(!x) return 0;
+    
+    i = 0x5f375a86- (i>>1); // beautiful number
+    x = *(float*)&i; 
+    x = x*(1.5f-xhalf*x*x); // 牛顿迭代法，提高精度
+    x = x*(1.5f-xhalf*x*x); // 牛顿迭代法，提高精度
+    x = x*(1.5f-xhalf*x*x); // 牛顿迭代法，提高精度
+
+    return 1/x;
+}
+
+inline long long min(const long long& x,const long long& y)
+{
+	return x>y?y:x;
+}
+
+inline long long abs(const long long& x)
+{
+	return x>0?x:-x;
+}
 struct node{
-	int x,y;
+	long long x,y;
 };
 inline void get(node &x)
 {
 	scanf("%d%d",&x.x,&x.y);
 	return;
 }
-double dis(node x,node y)
+inline double dis(const node& x,const node& y)
 {
-	int a=abs(x.x-y.x),b=abs(x.y-y.y);
+	long long a=abs(x.x-y.x),b=abs(x.y-y.y);
 	return sqrt(a*a+b*b);
 }
 node a[200010];
@@ -22,10 +45,10 @@ int main()
 	double ans=2000000000.0;
 	int n;
 	scanf("%d",&n);
-	for(int i=1;i<=n;i++)
+	for(register int i=1;i<=n;i++)
 		get(a[i]);
-	for(int i=1;i<=n;i++)
-		for(int j=i+1;j<=n;j++)
+	for(register int i=1;i<=n;i++)
+		for(register int j=i+1;j<=n;j++)
 			ans=min(ans,dis(a[i],a[j]));
 	printf("%.4f\n",ans);
 	return 0;
