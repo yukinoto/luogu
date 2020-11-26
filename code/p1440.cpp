1657a9f9739f1c56455b3dc17c7a7519cd601426
yukinoto <2733687567@qq.com>
2020-11-26 19:39:16 +0800

    .

--- code/p1440.cpp
+++ code/p1440.cpp
@@ -1,3 +1,22 @@
 #include <stdio.h>
 
+int ___[2000010],p[2000010];
+int top,front;
 int main()
+{
+	int n,m;
+	scanf("%d%d",&n,&m);
+	for(register int i=1;i<=n;i++)
+	{
+		int x;
+		scanf("%d",&x);
+		while(p[front]<i-m)
+			++front;
+		printf("%d\n",___[front]);
+		while(top>front&&___[top-1]>=x)
+			--top;
+		___[top]=x;
+		p[top++]=i;
+	}
+	return 0;
+}
