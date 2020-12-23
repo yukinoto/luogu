137bb86f80c96d8c11659b31e109c37773db0de9
yukinoto <2733687567@qq.com>
2020-12-23 17:23:10 +0800

    .

---
+++ code/p3373-2.cpp
@@ -0,0 +1,197 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int n,m,num[100010];
+
+#define a num
+typedef long long tpe;
+class tree{
+	protected:
+		int l,r;
+		class node{
+			protected:
+				node *ls,*rs;
+				int l,r;//[)
+				int tagtype;
+				tpe sum,tag;
+			public:
+				node(int left,int right)
+				{
+					l=left,r=right;
+					if(l==r-1)
+					{
+						ls=rs=nullptr;
+						sum=a[l];
+						tag=0;
+						return;
+					}
+					int mid=(l+r)/2;
+					if(mid>l)
+						ls=new node(left,mid);
+					else
+						ls=nullptr;
+					if(r>mid)
+						rs=new node(mid,right);
+					else
+						rs=nullptr;
+					sum=ls->sum+rs->sum;
+					tag=0;
+					tagtype=0;
+					return;
+				}
+				~node()
+				{
+					if(ls!=nullptr)
+						delete ls;
+					if(rs!=nullptr)
+						delete rs;
+					return;
+				}
+				void pushdown()
+				{
+					if(this->tagtype==1)
+					{
+						if(ls!=nullptr)
+						{
+							if(ls->tagtype!=1)
+							{
+								ls->pushdown();
+								ls->tagtype=1;
+								ls->tag=0;
+							}
+							ls->tag+=this->tag;
+							ls->sum+=(ls->r-ls->l)*this->tag;
+						}
+						if(rs!=nullptr)
+						{
+							if(rs->tagtype!=1)
+							{
+								rs->pushdown();
+								rs->tagtype=1;
+								rs->tag=0;
+							}
+							rs->tag+=this->tag;
+							rs->sum+=(ls->r-ls->l)*this->tag;
+						}
+						this->tag=0;
+						this->tagtype=0;
+					}
+					if(this->tagtype==2)
+					{
+						if(ls!=nullptr)
+						{
+							if(ls->tagtype!=2)
+							{
+								ls->pushdown();
+								ls->tagtype=1;
+								ls->tag=2;//tbd
+							}
+							ls->tag+=this->tag;
+							ls->sum+=(ls->r-ls->l)*this->tag;
+						}
+						if(rs!=nullptr)
+						{
+							if(rs->tagtype!=1)
+							{
+								rs->pushdown();
+								rs->tagtype=1;
+								rs->tag=0;
+							}
+							rs->tag+=this->tag;
+							rs->sum+=(ls->r-ls->l)*this->tag;
+						}
+						this->tag=0;
+						this->tagtype=0;
+					}
+				}
+				void add(int left,int right,int x)
+				{
+					if(left>=r||right<=l)
+						return;
+					if(left<=l&&right>=r)
+					{
+						this->tag+=x;
+						this->sum+=(r-l)*x;
+						return;
+					}
+					if(tag!=0)
+						this->pushdown();
+					this->sum=0;
+					if(ls!=nullptr)
+					{
+						ls->add(left,right,x);
+						this->sum+=ls->sum;
+					}
+					if(rs!=nullptr)
+					{
+						rs->add(left,right,x);
+						this->sum+=rs->sum;
+					}
+					return;
+				}
+				tpe quest(int left,int right)
+				{
+					if(left>=r||right<=l)
+						return 0;
+					if(left<=l&&right>=r)
+						return this->sum;
+					if(this->tag!=0)
+						this->pushdown();
+					tpe ans=0;
+					if(ls!=nullptr)
+						ans+=ls->quest(left,right);
+					if(rs!=nullptr)
+						ans+=rs->quest(left,right);
+					return ans;
+				}
+		};
+		node *root;
+	public:
+		tree()
+		{
+			//flexible
+			l=0,r=n;
+			root=new node(l,r);
+		}
+		~tree()
+		{
+			delete root;
+			return;
+		}
+		void add(int left,int right,int x)
+		{
+			root->add(left,right,x);
+			return;
+		}
+		tpe quest(int left,int right)
+		{
+			return root->quest(left,right);
+		}
+};
+
+int main()
+{
+	scanf("%d%d",&n,&m);
+	for(int i=0;i<n;i++)
+		scanf("%d",num+i);
+	tree *tr=new tree();
+	for(int i=0;i<m;i++)
+	{
+		static int f;
+		scanf("%d",&f);
+		if(f==1)
+		{
+			static int l,r,x;
+			scanf("%d%d%d",&l,&r,&x);
+			tr->add(l-1,r,x);
+		}
+		if(f==2)
+		{
+			static int l,r;
+			scanf("%d%d",&l,&r);
+			printf("%lld\n",tr->quest(l-1,r));
+		}
+	}
+	return 0;
+}
