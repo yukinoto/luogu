ba9565dd8a6b171c8bc3b2103245c165d0e456b3
yukinoto <2733687567@qq.com>
2021-01-13 16:53:58 +0800

    .

--- code/p3811.cpp
+++ code/p3811.cpp
@@ -1,6 +1,36 @@
 //p3811
 #include <stdio.h>
 
+template<typename T>
+T read()
+{
+	T ans=0;
+	char x=getchar();
+	while(x<'0'||x>'9')
+		x=getchar();
+	while(x>='0'&&x<='9')
+	{
+		ans=ans*10+(x-'0');
+		x=getchar();
+	}
+	return ans;
+}
+
+template<typename T>
+void read(T &ans)
+{
+	ans=0;
+	char x=getchar();
+	while(x<'0'||x>'9')
+		x=getchar();
+	while(x>='0'&&x<='9')
+	{
+		ans=ans*10+(x-'0');
+		x=getchar();
+	}
+	return;
+}
+
 template<typename T1,typename T2>
 struct pair{
 	T1 x;
@@ -20,18 +50,7 @@ pair<T,T> ExE(T a,T b,const T &d)
 	if(b==0)
 		return make_pair(a/d,b);
 	pair<T,T> sol=ExE(b,a%b,d);
-	return make_pair(sol.y-a/b*sol.x,sol.x);
-}
-pair<int,int> ExE(int a,int b,const int &d)
-{
-	if(b==0)
-	{
-		printf("%d %d %d %d\n",a/d,b,a,b);
-		return make_pair(a/d,b);
-	}
-	pair<int,int> sol=ExE(b,a%b,d);
-	printf("%d %d %d %d\n",sol.y-a/b*sol.x,sol.x,a,b);
-	return make_pair(sol.y-a/b*sol.x,sol.x);
+	return make_pair(sol.y,sol.x-a/b*sol.y);
 }
 template <typename T>
 T eu(T a,T b)
@@ -49,8 +68,7 @@ pair<T,T> solu(T a,T b,const T &d)
 }
 int main()
 {
-	int n,p;
-	scanf("%d%d",&n,&p);
+	int n=read<int>(),p=read<int>();
 	for(int i=1;i<=n;i++)
 	{
 		static pair<int,int> ans;
