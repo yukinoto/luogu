ced7fef92a3423edccaaab13d2774ffd42dc4c2d
yukinoto <2733687567@qq.com>
2021-02-06 11:06:25 +0800

    .

--- code/p1402.cpp
+++ code/p1402.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int n,q,p;
 vector<int> mp[410];
-
+/*
 template <typename T>
 class queue{
 	private:
@@ -18,7 +18,7 @@ class queue{
 					x=_x;
 					nxt=_nxt;
 				}
-				node(node *nxt)
+				node(node *_nxt)
 				{
 					nxt=_nxt;
 				}
@@ -60,7 +60,7 @@ class queue{
 			return;
 		}
 };
-
+*/
 void init()
 {
 	int x;
@@ -93,27 +93,45 @@ int deepth[410];
 bool bfs()
 {
 	memset(deepth,0,sizeof(deepth));
-	queue<int> q;
-	q.push(0);
+	queue<int> qu;
+	qu.push(0);
 	deepth[0]=1;
-	while(!q.empty())
+	while(!qu.empty())
 	{
-		int root=q.back();
+		int root=qu.back();
 		for(auto i:mp[root])
 		{
 			if(deepth[i]==0)
 			{
 				deepth[i]=deepth[root]+1;
-				q.push(i);
+				qu.push(i);
 			}
 		}
 	}
 	return deepth[p+q+n*2+1]!=0;
 }
 
-
+int dfs(int root,int mx)
+{
+	if(mx==0)
+		return 0;
+	if(root==p+q+n*2+1)
+		return mx;
+	int sum=0;
+	for(auto i:mp[root])
+	{
+		if(deepth[i]==deepth[root]+1)
+			sum+=dfs(i,min(mx,1));
+	}
+	return sum;
+}
 
 int main()
 {
 	init();
+	int ans=0;
+	while(bfs())
+		ans+=dfs(0,1);
+	printf("%d\n",ans);
+	return 0;
 }
