da78f77e3fff216d8b942a1ef15cbe501463fce6
yukinoto <2733687567@qq.com>
2021-02-06 10:59:20 +0800

    .

--- code/p1402.cpp
+++ code/p1402.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <vector>
 using namespace std;
 
@@ -48,7 +49,7 @@ class queue{
 			begin=begin->nxt=new node(x,nullptr);
 			return;
 		}
-		T end()
+		T back()
 		{
 			return end->x;
 		}
@@ -64,12 +65,16 @@ void init()
 {
 	int x;
 	scanf("%d%d%d",&n,&p,&q);
+	for(int i=1;i<=p;i++)
+		mp[0].push_back(i);
+	for(int i=p+n*2+1;i<=p+n*2+q;i++)
+		mp[i].push_back(p+n*2+q+1);
 	for(int k=1;k<=n;k++)
 		for(int i=1;i<=p;i++)
 		{
 			scanf("%d",&x);
 			if(x==1)
-				mp[i].push_back(k);
+				mp[i].push_back(k+p);
 		}
 	for(int k=1;k<=n;k++)
 	{
@@ -81,6 +86,29 @@ void init()
 				mp[q+n+k].push_back(n*2+p+i);
 		}
 	}
+}
+
+int deepth[410];
+
+bool bfs()
+{
+	memset(deepth,0,sizeof(deepth));
+	queue<int> q;
+	q.push(0);
+	deepth[0]=1;
+	while(!q.empty())
+	{
+		int root=q.back();
+		for(auto i:mp[root])
+		{
+			if(deepth[i]==0)
+			{
+				deepth[i]=deepth[root]+1;
+				q.push(i);
+			}
+		}
+	}
+	return deepth[p+q+n*2+1]!=0;
 }
 
 
