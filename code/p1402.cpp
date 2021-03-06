b243191e5c89d51eb15609fad775932aa2577f6f
yukinoto <2733687567@qq.com>
2021-03-06 14:36:30 +0800

    .

--- code/p1402.cpp
+++ code/p1402.cpp
@@ -1,66 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <vector>
+#include <queue>
 using namespace std;
 
 int n,q,p;
 vector<int> mp[410];
-/*
-template <typename T>
-class queue{
-	private:
-		class node{
-			public:
-				T x;
-				node *nxt;
-				node(const T &_x,node *_nxt)
-				{
-					x=_x;
-					nxt=_nxt;
-				}
-				node(node *_nxt)
-				{
-					nxt=_nxt;
-				}
-				node()
-				{
-					nxt=nullptr;
-				}
-		};
-		node *begin,*end;
-	public:
-		queue()
-		{
-			begin=end=nullptr;
-		}
-		inline bool empty()
-		{
-			return end==nullptr;
-		}
-		void pop()
-		{
-			node *pt=end->nxt;
-			delete end;
-			end=pt;
-			return;
-		}
-		void push(const T &x)
-		{
-			begin=begin->nxt=new node(x,nullptr);
-			return;
-		}
-		T back()
-		{
-			return end->x;
-		}
-		~queue()
-		{
-			while(!empty())
-				pop();
-			return;
-		}
-};
-*/
+
 void init()
 {
 	int x;
@@ -99,6 +45,7 @@ bool bfs()
 	while(!qu.empty())
 	{
 		int root=qu.back();
+		qu.pop();
 		for(auto i:mp[root])
 		{
 			if(deepth[i]==0)
