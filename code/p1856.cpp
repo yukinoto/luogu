6e597cdb7b5053e4bf2b9451a633ea42f28feb61
yukinoto <2733687567@qq.com>
2021-03-10 16:52:39 +0800

    .

--- code/p1856.cpp
+++ code/p1856.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <algorithm>
 #include <utility>
+#include <vector>
+#include <set>
 using namespace std;
 
 class line{
@@ -10,7 +12,7 @@ class line{
 		line(){}
 		bool operator<(const line&x)
 		{
-			return y<x.y;
+			return y==x.y?(x1==x.x1?x2<x.x2:x1<x.x1):y<x.y;
 		}
 		line(int _x1,int _x2,int _y,int _type)
 		{
@@ -34,6 +36,112 @@ pair<line,line> gen2(const squ &x)
 	return make_pair(line(x.y1,x.y2,x.x1,1),line(x.y1,x.y2,x.x2,-1));
 }
 
+namespace tr{
+	vector<long long> pt;
+	class tree{
+		private:
+			class node{
+				private:
+					long long l,r,len,rpt,rpttag;
+					node *lc,*rc;
+				public:
+					node(long long left,long long right)
+					{
+						l=left,r=right;
+						len=0;
+						rpt=rpttag=0;
+						if(left>=right-1)
+						{
+							lc=rc=nullptr;
+							return;
+						}
+						long long mid=(left+right)/2;
+						lc=new node(left,mid),rc=new node(mid,right);
+						return;
+					}
+					~node()
+					{
+						delete lc,rc;
+					}
+					void pushtag()
+					{
+						if(rpttag==0)
+							return;
+						auto f=[this](node *ptr)
+						{
+							if(ptr==nullptr)
+								return;
+							ptr->rpt+=rpttag;
+							ptr->rpttag+=rpttag;
+							return;
+						};
+						f(lc),f(rc);
+						rpttag=0;
+						return;
+					}
+					long long quest(long long left,long long right)
+					{
+						if(lc==nullptr)
+							return rpt==0?0:len=pt[r]-pt[l];
+						if(left>=r||right<=l||(left>=right-1&&rpt==0))
+							return 0;
+						pushtag();
+						if(left<=l&&right>=r)
+						{
+							if(rpt!=0)
+								return len=pt[r]-pt[l];
+							else
+								return len=lc->quest(left,right)+rc->quest(left,right);
+						}
+						return len=lc->quest(left,right)+rc->quest(left,right);
+					}
+					void add(long long left,long long right,long long x)
+					{
+						if(left>=r||right<=l)
+							return;
+						pushtag();
+						if(left<=l&&right>=r)
+						{
+							rpt+=x;
+							rpttag+=x;
+							return;
+						}
+						lc->add(left,right,x),rc->add(left,right,x);
+						return;
+					}
+			};
+			node *root;
+		public:
+			tree(long long left,long long right)
+			{
+				root=new node(left,right);
+			}
+			~tree(){delete root;}
+			void add(long long left,long long right,long long x)
+			{
+				root->add(left,right,x);
+			}
+			void add(long long first,long long second,int type)
+			{
+				root->add(first,second,type);
+			}
+			long long quest()
+			{
+				return root->quest(0,pt.size()-1);
+			}
+	};
+}
+
+long long sch(long long x,long long left,long long right)
+{
+	if(left>=right-1)
+		return left;
+	if(tr::pt[(left+right)/2]>x)
+		return sch(x,left,(left+right)/2);
+	else
+		return sch(x,(left+right)/2,right);
+}
+
 int main()
 {
 	int n;
@@ -41,6 +149,7 @@ int main()
 	squ *pt=new squ[n];
 	for(int i=0;i<n;i++)
 		pt->get();
+	long long ans=0l;
 	auto f=[&](decltype(gen1) gen)->void
 	{
 		line *x=new line[n<<1];
@@ -50,6 +159,34 @@ int main()
 			x[i<<1]=ct.first,x[(i<<1)^1]=ct.second;
 		}
 		sort(x,x+(n<<1));
-		
+		set<long long>nodes;
+		for(int i=0;i<(n<<1);i++)
+		{
+			nodes.insert(x[i].x1),
+			nodes.insert(x[i].x2);
+		}
+		tr::pt.clear();
+		for(auto it=nodes.begin();it!=nodes.end();++it)
+		{
+			tr::pt.push_back(*it);
+		}
+		for(int i=0;i<=(n<<1);i++)
+		{
+			x[i].x1=sch(x[i].x1,0,tr::pt.size());
+			x[i].x2=sch(x[i].x2,0,tr::pt.size());
+		}
+		tr::tree *t=new tr::tree(0,tr::pt.size());
+		long long bef=0l;
+		for(int i=0;i<=(n<<1);i++)
+		{
+			t->add(x[i].x1,x[i].x2,x[i].type);
+			long long now=t->quest();
+			ans+=abs(bef-now);
+			bef=now;
+		}
+		delete t;
+		delete[] x;
 	};
+	printf("%lld",ans);
+	return 0;
 }
