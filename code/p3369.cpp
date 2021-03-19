debd37c93a1938868c87141ecddd02be530a8cd1
yukinoto <2733687567@qq.com>
2021-03-19 20:44:21 +0800

    .

--- code/p3369.cpp
+++ code/p3369.cpp
@@ -227,6 +227,7 @@ class treap{
 							{
 								pt1->fa=tf;
 								merge(pt1->rc,pt2,pt1);
+								pt1->resetsons();
 								return pt1;
 							}
 						}
@@ -243,6 +244,7 @@ class treap{
 							{
 								pt1->fa=tf;
 								merge(pt1->lc,pt2,pt1);
+								pt1->resetsons();
 								return pt1;
 							}
 						}
@@ -262,6 +264,7 @@ class treap{
 							{
 								pt2->fa=tf;
 								merge(pt2->rc,pt1,pt2);
+								pt2->resetsons();
 								return pt2;
 							}
 						}
@@ -278,6 +281,7 @@ class treap{
 							{
 								pt2->fa=tf;
 								merge(pt2->lc,pt1,pt2);
+								pt2->resetsons();
 								return pt2;
 							}
 						}
@@ -373,7 +377,7 @@ class treap{
 			}
 			void del(const T &x)
 			{
-				(root->get(x))->del();
+				root=(root->get(x))->del();
 				return;
 			}
 			T nth(int n)
@@ -440,7 +444,7 @@ void f6(treap<int> *tr)
 decltype(f1) *f[6]={f1,f2,f3,f4,f5,f6};
 int main()
 {
-	freopen("./code/p3369.in","r",stdin);
+	//freopen("./code/p3369.in","r",stdin);
 	int n;
 	scanf("%d",&n);
 	auto* tr=new treap<int>;
