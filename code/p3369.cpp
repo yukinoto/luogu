4ef85912a4cedb8e63b508a96650fa4c4d4cd37b
yukinoto <2733687567@qq.com>
2021-03-19 20:27:09 +0800

    .

--- code/p3369.cpp
+++ code/p3369.cpp
@@ -32,7 +32,7 @@ class treap{
 					if(rc!=nullptr)
 					{
 						rc->sons=0;
-						if(rc->rc!=nullptr)
+						if(rc->lc!=nullptr)
 							rc->sons+=rc->lc->sons;
 						if(rc->rc!=nullptr)
 							rc->sons+=rc->rc->sons;
@@ -40,18 +40,76 @@ class treap{
 						sons+=rc->sons;
 					}
 					sons++;
+					if(fa!=nullptr)
+						fa->resetsons();
 				}
 				void splay(bool type)
 				{
-					decltype(lc) fa_=fa,lc_=lc,rc_=rc,lc_rc_=lc->rc,rc_lc_=rc->lc;
 					if(type==LEFT_UP)
 					{
-						fa=lc_,lc=lc_rc_,lc_->rc=this,lc_->fa=fa_;
-					}
-					if(type=RIGHT_UP)
-					{
-						fa=rc_,rc=rc_lc_,rc_->lc=this,rc_->fa=fa_;
-					}
+						if(lc==nullptr)
+							return;
+						if(lc->rc==nullptr)
+						{
+							lc->fa=fa;
+							if(fa!=nullptr)
+								if(fa->lc==this)
+									fa->lc=lc;
+								else
+									fa->rc=lc;
+							fa=lc;
+							lc->rc=this;
+							lc=nullptr;
+							fa->resetsons();
+							return;
+						}
+						else
+						{
+							lc->fa=fa;
+							if(fa!=nullptr)
+								if(fa->lc==this)
+									fa->lc=lc;
+								else
+									fa->rc=lc;
+							auto pt=lc;
+							lc=pt->rc;
+							pt->rc->fa=this;
+							pt->rc=this;
+							fa=pt;
+						}
+					}else if(type==RIGHT_UP)
+						{
+							if(rc==nullptr)
+								return;
+							if(rc->lc==nullptr)
+							{
+								rc->fa=fa;
+								if(fa!=nullptr)
+									if(fa->lc==this)
+										fa->lc=rc;
+									else
+										fa->rc=rc;
+								fa=rc;
+								rc->lc=this;
+								rc=nullptr;
+								fa->resetsons();
+								return;
+							}
+							else
+							{
+								rc->fa=fa;
+								if(fa!=nullptr)
+									if(fa->lc==this)
+										fa->lc=rc;
+									else
+										fa->rc=rc;
+								auto pt=rc;
+								rc=pt->lc;
+								pt->lc->fa=this;
+								pt->lc=this;
+								fa=pt;
+							}
+						}
 					fa->resetsons();
 				}
 				void mkHeap()
@@ -66,14 +124,6 @@ class treap{
 						{
 							fa->splay(RIGHT_UP);
 						}
-					}
-					if(key==0x7fffffff)
-					{
-						if(fa->lc==this)
-							fa->lc=nullptr;
-						else
-							fa->rc=nullptr;
-						delete this;
 					}
 					return;
 				}
@@ -96,6 +146,21 @@ class treap{
 						return rc->get(x);
 					}
 				}
+				auto checknth(const T &x,int bef)const ->int
+				{
+					if(num==x)
+						return bef+1+(lc==nullptr?0:lc->sons);
+					if(num>x)
+						if(lc==nullptr)
+							return -1;
+						else
+							return lc->checknth(x,bef);
+					else
+						if(rc==nullptr)
+							return -1;
+						else
+							return rc->checknth(x,bef+1+(lc==nullptr?0:lc->sons));
+				}
 				node(const T &x,decltype(lc) _fa)
 				{
 					fa=_fa;
@@ -114,7 +179,7 @@ class treap{
 						lc->lc=_lc;
 						lc->mkHeap();
 					}
-					if(x<num)
+					if(x>num)
 					{
 						if(rc==nullptr)
 						{
@@ -135,11 +200,117 @@ class treap{
 							lc->add(x);
 					}
 				}
-				void del()
+				auto merge(decltype(lc) pt1,decltype(lc) pt2,decltype(lc) tf)->decltype(lc)
 				{
-					key=0x7fffffff;
+					if(pt1==nullptr)
+					{
+						pt2->fa=tf;
+						return pt2;
+					}
+					if(pt2==nullptr)
+					{
+						pt1->fa=tf;
+						return pt1;
+					}
+					if(pt1->key<pt2->key)
+					{
+						if(pt1->num<pt2->num)
+						{
+							if(pt1->rc==nullptr)
+							{
+								pt1->rc=pt2;
+								pt1->fa=tf;
+								pt2->fa=pt1;
+								return pt1;
+							}
+							else
+							{
+								pt1->fa=tf;
+								merge(pt1->rc,pt2,pt1);
+								return pt1;
+							}
+						}
+						else
+						{
+							if(pt1->lc==nullptr)
+							{
+								pt1->lc=pt2;
+								pt1->fa=tf;
+								pt2->fa=pt1;
+								return pt1;
+							}
+							else
+							{
+								pt1->fa=tf;
+								merge(pt1->lc,pt2,pt1);
+								return pt1;
+							}
+						}
+					}
+					else
+					{
+						if(pt2->num<pt1->num)
+						{
+							if(pt2->rc==nullptr)
+							{
+								pt2->rc=pt1;
+								pt2->fa=tf;
+								pt1->fa=pt2;
+								return pt2;
+							}
+							else
+							{
+								pt2->fa=tf;
+								merge(pt2->rc,pt1,pt2);
+								return pt2;
+							}
+						}
+						else
+						{
+							if(pt2->lc==nullptr)
+							{
+								pt2->lc=pt1;
+								pt2->fa=tf;
+								pt1->fa=pt2;
+								return pt2;
+							}
+							else
+							{
+								pt2->fa=tf;
+								merge(pt2->lc,pt1,pt2);
+								return pt2;
+							}
+						}
+					}
+				}
+				auto del()->decltype(lc)
+				{
+					key=0x80000000;
 					mkHeap();
-					return;
+					if(lc==nullptr&&rc==nullptr)
+					{
+						delete this;
+						return nullptr;
+					}
+					if(lc==nullptr)
+					{
+						decltype(lc) ans=rc;
+						rc->fa=nullptr;
+						rc=nullptr;
+						delete this;
+						return ans;
+					}
+					if(rc==nullptr)
+					{
+						decltype(lc) ans=lc;
+						lc->fa=nullptr;
+						lc=nullptr;
+						delete this;
+						return ans;
+					}
+					decltype(lc) l=lc,r=rc;
+					lc=rc=nullptr;delete this;
+					return merge(l,r,nullptr);
 				}
 				auto fdnth(int n,int bef)->decltype(lc)
 				{
@@ -154,6 +325,38 @@ class treap{
 					else
 						return rc->fdnth(n,bef+1+lc->sons);
 				}
+				bool isRoot()const{return fa==nullptr;}
+				auto gtf()const ->decltype(fa){return fa;}
+				T bef(const T &x)const
+				{
+					if(lc==nullptr&&rc==nullptr)
+						return num;
+					if(num<x)
+						if(rc==nullptr)
+							return num;
+						else
+							return rc->bef(x);
+					else
+						if(lc==nullptr)
+							return 0x7fffffff;
+						else
+							return lc->bef(x);
+				}
+				T nxt(const T &x)const
+				{
+					if(lc==nullptr&&rc==nullptr)
+						return num;
+					if(num>x)
+						if(lc==nullptr)
+							return num;
+						else
+							return lc->nxt(x);
+					else
+						if(rc==nullptr)
+							return 0x7fffffff;
+						else
+							return rc->nxt(x);
+				}
 		};
 		node *root;
 		public:
@@ -165,15 +368,29 @@ class treap{
 					root=new node(x,nullptr);
 				else
 					root->add(x);
+				while(!root->isRoot())
+					root=root->gtf();
 			}
 			void del(const T &x)
 			{
-				root->get(x)->del();
+				(root->get(x))->del();
 				return;
 			}
 			T nth(int n)
 			{
 				return root->fdnth(n,0)->get();
+			}
+			int checkth(const T &x)const
+			{
+				return root->checknth(x,0);
+			}
+			T bef(const T &x)const
+			{
+				return root->bef(x);
+			}
+			T nxt(const T &x)const
+			{
+				return root->nxt(x);
 			}
 };
 
@@ -191,17 +408,39 @@ void f2(treap<int> *tr)
 	tr->del(x);
 	return;
 }
-
+void f3(treap<int> *tr)
+{
+	int x;
+	scanf("%d",&x);
+	printf("%d\n",tr->checkth(x));
+	return;
+}
 void f4(treap<int> *tr)
 {
 	int x;
 	scanf("%d",&x);
 	printf("%d\n",tr->nth(x));
+	return;
+}
+void f5(treap<int> *tr)
+{
+	int x;
+	scanf("%d",&x);
+	printf("%d\n",tr->bef(x));
+	return;
+}
+void f6(treap<int> *tr)
+{
+	int x;
+	scanf("%d",&x);
+	printf("%d\n",tr->nxt(x));
+	return;
 }
 
-decltype(f1) *f[6]={f1,f2,nullptr,f4,nullptr,nullptr};
+decltype(f1) *f[6]={f1,f2,f3,f4,f5,f6};
 int main()
 {
+	freopen("./code/p3369.in","r",stdin);
 	int n;
 	scanf("%d",&n);
 	auto* tr=new treap<int>;
