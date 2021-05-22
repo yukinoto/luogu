28833455a29d564de759a08fc50139a00e422b17
yukinoto <2733687567@qq.com>
2021-05-22 09:52:51 +0800

    .

---
+++ code/p2613.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+template<typename T1,typename T2>
+struct pair{
+	T1 x;
+	T2 y;
+};
+template<typename T1,typename T2>
+inline pair<T1,T2> make_pair(T1 _x,T2 _y)
+{
+	pair<T1,T2> ans;
+	ans.x=_x,ans.y=_y;
+	return ans;
+}
+
+template <typename T>
+pair<T,T> ExE(T a,T b,const T &d)
+{
+	if(b==0)
+		return make_pair<T>(a/d,b);
+	pair<T,T> sol=ExE(b,a%b,d);
+	return make_pair<T>(sol.y,sol.x-a/b*sol.y);
+}
+template <typename T>
+T eu(T a,T b)
+{
+	if(b==0)
+		return a;
+	return eu(b,a%b);
+}
+template <typename T>
+pair<T,T> solu(T a,T b,const T &d)
+{
+	if(d%eu(a,b)!=0)
+		return make_pair<T>((T)0,(T)0);
+	return ExE(a,b,d);
+}
+template <typename T>
+T anti(T a,T p)
+{
+	return (solu<T>(a,p,1).x%p+p)%p;
+}
+
+int main()
+{
+	long long a,b,mod=19260817;
+	scanf("%lld%lld",&a,&b);
+	printf("%lld\n",a*anti(b,mod)%mod);
+}
