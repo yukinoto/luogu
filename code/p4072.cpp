5ff4ae324d4d7fb55550a0cb3333d163790e4f24
yukinoto <2733687567@qq.com>
2021-10-15 21:21:59 +0800

    .

--- code/p4072.cpp
+++ code/p4072.cpp
@@ -6,12 +6,14 @@ using namespace std;
 typedef int Int;
 
 Int n,m;
-Int *p,*f;
-const Int inf=0x7fffffff;
+Int *p,**f;
+const Int inf=0x3fffffff;
 
 void del()
 {
-	delete[]p,f;
+	delete[]p;
+	delete[]f[0],f[1];
+	delete[]f;
 	return;
 }
 
@@ -19,28 +21,37 @@ void init()
 {
 	cin>>n>>m;
 	p=new Int[n];
-	f=new Int[n];
+	f=new Int*[2];
+	f[0]=new Int[n],f[1]=new Int[n];
 	cin>>p[0];
 	for(int i=1;i<n;i++)
 	{
 		Int x;
 		cin>>x;
 		p[i]=p[i-1]+x;
-		f[i]=inf;
+		f[1][i]=inf;
 	}
-	f[0]=inf;
+	f[1][0]=inf;
 	atexit(del);
 	return;
 }
 
 void work()
 {
-	for(int i=0;i<n;i++)
+	for(int i=0;i<m;i++)
 	{
-		for(int j=0;j<i;j++)
-			f[i]=min(f[i],f[j]+p[j]*p[j]-2*p[i]*p[j]);
-		f[i]=min(f[i],p[i]*p[i]);
+		for(int j=0;j<n;j++)
+		{
+			f[i&1][j]=f[(i&1)^1][j];
+			for(int k=0;k<j;k++)
+			{
+				f[i&1][j]=min(f[i&1][j],f[(i&1)^1][k]+p[j]*p[j]-2*p[i]*p[j]);
+			}
+			f[i&1][j]=min(f[i&1][j],p[j]*p[j]);
+		}
 	}
+	cout<<f[(m-1)&1][n-1]<<endl;
+	return;
 }
 
 int main()
