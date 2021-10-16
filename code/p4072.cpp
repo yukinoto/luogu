40669cf3bb6ea8e07db96c946572fc93ca168995
yukinoto <2733687567@qq.com>
2021-10-16 09:16:51 +0800

    .

--- code/p4072.cpp
+++ code/p4072.cpp
@@ -20,10 +20,10 @@ void del()
 
 void init()
 {
-	//cin.sync_with_stdio(false);
-	//cout.sync_with_stdio(false);
-	//cin.tie(nullptr);
-	//cout.tie(nullptr);
+	cin.sync_with_stdio(false);
+	cout.sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
 	cin>>n>>m;
 	p=new Int[n];
 	f=new Int*[2];
@@ -41,26 +41,97 @@ void init()
 	return;
 }
 
+template<typename T>
+class queue{
+	private:
+		T a[3001];
+	public:
+		int first,last;
+		queue()
+		{
+			first=last=0;
+		}
+		void push(const T &x)
+		{
+			a[last++]=x;
+		}
+		void popback()
+		{
+			last--;
+		}
+		void popfront()
+		{
+			first++;
+		}
+		void clear()
+		{
+			first=last=0;
+		}
+		bool empty()
+		{
+			return first>=last;
+		}
+		int size()
+		{
+			return last-first;
+		}
+		T&operator [](int p)
+		{
+			return a[p];
+		}
+};
+
+struct pt{
+	Int x,y;
+};
+
+pt mkpt(Int _x,Int _y)
+{
+	pt ans;
+	ans.x=_x,ans.y=_y;
+	return ans;
+}
+
+queue<pt> q;
+
+void insert(pt x)
+{
+	while(q.size()>1)
+	{
+		if((long long)(q[q.last-1].y-q[q.last-2].y)*(q[q.last-1].x-x.x)<(long long)(q[q.last-1].y-x.y)*(q[q.last-1].x-q[q.last-2].x))
+			q.popback();
+		else
+			break;
+	}
+	q.push(x);
+	return;
+}
+
+int e;
+Int fnd(int s)
+{
+	auto f=[&](pt x)
+	{
+		return x.y-2*p[s]*x.x;
+	};
+	while(e+1<q.last&&f(q[e+1])<f(q[e]))
+		e++;
+	return f(q[e]);
+}
+
 void work()
 {
 	for(int j=0;j<n;j++)
 		f[0][j]=p[j]*p[j];
 	for(int i=1;i<m;i++)
 	{
+		q.clear();
+		e=0;
+		insert(mkpt(p[i-1],f[(i&1)^1][i-1]+p[i-1]*p[i-1]));
 		for(int j=i;j<n;j++)
 		{
-			f[i&1][j]=inf;
-			for(int k=0;k<j;k++)
-			{
-				if(f[(i&1)^1][k]!=inf)
-				{
-					f[i&1][j]=min(f[i&1][j],f[(i&1)^1][k]+p[k]*p[k]-2*p[k]*p[j]);
-				}
-			}
-			if(f[i&1][j]!=inf)
-			{
-				f[i&1][j]+=p[j]*p[j];
-			}
+			f[i&1][j]=fnd(j)+p[j]*p[j];
+			insert(mkpt(p[j],f[(i&1)^1][j]+p[j]*p[j]));
 		}
 	}
 	cout<<f[(m&1)^1][n-1]*m-p[n-1]*p[n-1]<<endl;
