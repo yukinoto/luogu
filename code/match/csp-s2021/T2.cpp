40f766b45ca7c2828324ad0c981f04f5249a9842
yukinoto <2733687567@qq.com>
2021-10-31 08:23:59 +0800

    .

--- code/match/csp-s2021/T2.cpp
+++ code/match/csp-s2021/T2.cpp
@@ -10,6 +10,37 @@ long long f[510][510],g[510][510];
 //long long ans[510];
 string x;
 
+/*template<typename T>
+T quickpow(T x,long long n)
+{
+	long long a=0;
+	long long cnt=0;
+	while(n!=0)
+	{
+		a<<=1;
+		a|=(n&1);
+		n>>=1;
+		++cnt;
+	}
+	T ans=1;
+	for(;cnt>0;--cnt)
+	{
+		ans=ans*ans%mod;
+		if(a&1)
+		{
+			ans=ans*x%mod;
+		}
+        a>>=1;
+	}
+	return ans;
+}
+
+template<typename T>
+T anti(T x)
+{
+	return quickpow(x,mod-2);
+}*/
+
 int main()
 {
 	cin>>n>>k>>x;
@@ -17,11 +48,11 @@ int main()
 	for(int i=0;i<n;i++)
 	{
 		if(x[i]=='('||x[i]=='?')
-			for(int j=i+1;j<min(n,i+k+1);j++)
+			for(int j=i+1;j<=min(n-1,i+k+1);j++)
 			{
 				if(x[j]==')'||x[j]=='?')
 					f[i][j]=1;
-				if(x[j]==')')
+				if(x[j]==')'||x[j]=='(')
 					break;
 			}
 	}
@@ -30,18 +61,45 @@ int main()
 		for(int i=0;i+l<n;i++)
 			if((x[i]=='('||x[i]=='?')&&(x[i+l]=='?'||x[i+l]==')'))
 			{
-				//case 1:ASB
-				for(int j=i+1;j<=i+l-3;j++)
+				//case 1:ASB+AB
+				/*for(int j=i+1;j<=i+l-2;j++)
 				{
-					if(x[j+1]=='?'||x[j+1]=='*')
+					//if(x[j+1]=='?'||x[j+1]=='*')
+					//{
+					for(int r=j+1;r<=min<long long>(i+l-1,j+k+1);r++)
 					{
-						for(int r=j+2;r<=min<long long>(i+l-1,j+k+1);r++)
+						g[i][i+l]=(g[i][i+l]+(f[i][j]+g[i][j])*f[r][i+l])%mod;
+						if(x[r]=='('||x[r]==')')
+							break;
+					}
+					//}
+				}*/
+				//case 1:ASB+AB
+				int r=i+1;
+				long long sm;
+				for(int j=i+1;j<=i+l-2;j++)
+				{
+					if(j==r)
+					{
+						sm=0;
+						for(r=j+1;;r++)
 						{
-							g[i][i+l]=(g[i][i+l]+(f[i][j]+g[i][j])*f[r][i+l])%mod;
-							if(x[r]=='('||x[r]==')')
+							sm=(sm+f[r][i+l])%mod;
+							if(x[r]=='('||x[r]==')'||r==min<long long>(i+l-1,j+k+1))
 								break;
 						}
 					}
+					else
+					{
+						sm-=f[j][i+l];
+						if(x[r]=='*'||x[r]=='?')
+						{
+							r++;
+							sm+=f[r][i+l];
+						}
+						sm=(sm%mod+mod)%mod;
+					}
+					g[i][i+l]=(g[i][i+l]+(f[i][j]+g[i][j])*sm)%mod;
 				}
 				//case 2:(SA)
 				if(x[i+1]=='*'||x[i+1]=='?')
@@ -62,8 +120,8 @@ int main()
 				//case 4:(A)
 				f[i][i+l]=(f[i][i+l]+f[i+1][i+l-1]+g[i+1][i+l-1])%mod;
 				//case 5:AB
-				for(int j=i+1;j<=i+l-2;j++)
-					g[i][i+l]=(g[i][i+l]+(f[i][j]+g[i][j])*f[j+1][i+l])%mod;
+				//for(int j=i+1;j<=i+l-2;j++)
+				//	g[i][i+l]=(g[i][i+l]+(f[i][j]+g[i][j])*f[j+1][i+l])%mod;
 			}
 	}
 	/*for(int i=1;i<n;i++)
