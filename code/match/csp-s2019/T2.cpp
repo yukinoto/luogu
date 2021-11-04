177ef0447f7817a00de95d3f76ff9f51f32c5bdc
yukinoto <2733687567@qq.com>
2021-11-04 13:42:02 +0800

    .

--- code/match/csp-s2019/T2.cpp
+++ code/match/csp-s2019/T2.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <stack>
+#include <vector>
 using namespace std;
 
 typedef long long Int;
 
 int n;
 char x[500010];
-Int dp[500010],fa[500010];//end in i
+Int dp[500010];
+int fa[500010];//end in i
+vector<int>sons[500010];
 
 stack<int> st;
 
@@ -69,14 +72,66 @@ namespace full{
 		for(int i=1;i<=n;i++)
 			cin>>x[i];
 		for(int i=2;i<=n;i++)
+		{
 			cin>>fa[i];
+			sons[fa[i]].push_back(i);
+		}
 		return;
+	}
+	void dfs(int root)
+	{
+		if(x[root]=='(')
+		{
+			st.push(fa[root]);
+			for(auto i:sons[root])
+			{
+				dfs(i);
+			}
+			st.pop();
+		}
+		else
+		{
+			if(!st.empty())
+			{
+				int p=st.top();
+				st.pop();
+				dp[root]=dp[p]+1;
+				for(auto i:sons[root])
+				{
+					dfs(i);
+				}
+				st.push(p);
+			}
+			else
+			{
+				for(auto i:sons[root])
+				{
+					dfs(i);
+				}
+			}
+		}
+		return;
+	}
+	Int ans=0;
+	void dfs1(int root,Int sum)
+	{
+		sum+=dp[root];
+		ans^=Int(root)*sum;
+		for(auto i:sons[root])
+			dfs1(i,sum);
+		return;
+	}
+	void work()
+	{
+		dfs(1);
+		dfs1(1,0);
+		cout<<ans<<endl;
 	}
 }
 
 int main()
 {
-	linear::init();
-	linear::work();
+	full::init();
+	full::work();
 	return 0;
 }
