4373f30cb10230844b88f1c77e7a42b598218b78
yukinoto <2733687567@qq.com>
2021-11-05 21:20:50 +0800

    .

--- code/match/csp-s2020/T3.cpp
+++ code/match/csp-s2020/T3.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <set>
 using namespace std;
 
@@ -45,17 +44,18 @@ ostream& operator <<(ostream& ous,modint x)
 	return ous<<x.x;
 }
 
-int n,m;
-int type[MAXN];
-modint p[MAXN],v[MAXN],a[MAXN],tms[MAXN];
+int n,m,t;
+int type[MAXN],que[MAXN],p[MAXN];
+modint v[MAXN],a[MAXN],nv[MAXN];
 vector<int>c[MAXN],at[MAXN];
-int idg[MAXN];
+int idg[MAXN],idg2[MAXN];
 
 void init()
 {
-	cin>>n>>m;
-	for(int i=0;i<=n;i++)
+	cin>>n;
+	for(int i=1;i<=n;i++)
 		cin>>a[i];
+	cin>>m;
 	for(int i=1;i<=m;i++)
 	{
 		cin>>type[i];
@@ -77,16 +77,82 @@ void init()
 				cin>>cache;
 				c[i].push_back(cache);
 				at[cache].push_back(i);
-				idg[cache]++;
+				idg[i]++;
 			}
+		}
+	}
+	cin>>t;
+	for(int i=1;i<=t;i++)
+		cin>>que[i];
+	return;
+}
+
+vector<int> q;
+int front=0;
+void tp1()
+{
+	for(int i=1;i<=m;i++)
+	{
+		if(idg[i]==0)
+			q.push_back(i);
+		if(type[i]==3)
+			nv[i]=modint(1);
+		if(type[i]==2)
+			nv[i]=v[i];
+		if(type[i]==1)
+			nv[i]=modint(1);
+	}
+	while(q.size()>front)
+	{
+		int p=q[front++];
+		for(auto i:at[p])
+		{
+			idg[i]--;
+			nv[i]*=nv[p];
+			if(idg[i]==0)
+				q.push_back(i);
 		}
 	}
 	return;
 }
-
-void tp1()
+modint dms[MAXN],add[MAXN],tag,tag1;
+void tp2()
 {
+	tag1=modint(1);
+	for(int i=t;i>0;i--)
+	{
+		dms[que[i]]+=tag1;
+		tag1*=nv[que[i]];
+	}
+	for(int i=q.size()-1;i>=0;i--)
+	{
+		int pt=q[i];
+		if(type[pt]==1)
+			add[p[pt]]+=dms[pt]*v[pt];
+		if(type[pt]==3)
+		{
+			tag=modint(1);
+			for(auto it=c[pt].rbegin();it!=c[pt].rend();it++)
+			{
+				dms[*it]+=tag;
+				tag*=nv[*it];
+			}
+		}
+	}
+}
+
+void work()
+{
+	tp1();
+	tp2();
+	for(int i=1;i<=n;i++)
+		cout<<a[i]*tag1+add[i]<<' ';
+	cout<<endl;
 }
 
 int main()
-{;}
+{
+	init();
+	work();
+	return 0;
+}
