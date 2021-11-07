73fdcd5ce2cdf13b4dad6fb02ac0b81cb426648b
yukinoto <2733687567@qq.com>
2021-11-07 17:05:14 +0800

    .

--- code/match/NOIP2018/p5022.cpp
+++ code/match/NOIP2018/p5022.cpp
@@ -58,6 +58,67 @@ namespace part{
 	}
 }
 
+namespace rol{
+	pair<int,int> ro;
+	void build(int root)
+	{
+		taken[root]=true;
+		for(auto i:edge[root])
+		{
+			if(i!=tr[root].fa&&!taken[i])
+			{
+				tr[i].fa=root;
+				tr[root].sons.push_back(i);
+				build(i);
+			}
+			if(i!=tr[root].fa&&taken[i])
+				ro=make_pair(i,root);
+		}
+		return;
+	}
+	void init()
+	{
+		cin>>n>>m;
+		for(int i=0;i<m;i++)
+		{
+			int x,y;
+			cin>>x>>y;
+			edge[x].push_back(y);
+			edge[y].push_back(x);
+		}
+		tr[1].fa=-1;
+		build(1);
+	}
+	bool check(int root)
+	{
+		int p=ro.second;
+		while(p!=ro.first&&p!=root)
+			p=tr[p].fa;
+		return p==root;
+	}
+	void work(int root)
+	{
+		cout<<root<<' ';
+		sort(tr[root].sons.begin(),tr[root].sons.end());
+		if(root!=ro.first)
+			for(auto i:tr[root].sons)
+			{
+				work(i);
+			}
+		else
+		{
+			
+		}
+		return;
+	}
+	void main()
+	{
+		init();
+		work(1);
+		return;
+	}
+}
+
 int main()
 {
 	part::main();
