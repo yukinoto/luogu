40747ad1bbdb67688fcf8b1cca74f5e7c287ccf5
yukinoto <2733687567@qq.com>
2021-11-09 13:39:48 +0800

    .

--- code/match/NOIP2018/p5024.cpp
+++ code/match/NOIP2018/p5024.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 struct node{
 	int fa;
 	vector<int>sons;
-	int dpf0,dpf1,dps0,dps1;
+	long long w;
+	long long dpf0,dpf1,dps0,dps1;
 };
 
 int n,m;
@@ -31,6 +33,8 @@ void init()
 {
 	string x;
 	cin>>n>>m>>x;
+	for(int i=1;i<=n;i++)
+		cin>>tr[i].w;
 	for(int i=1;i<n;i++)
 	{
 		int u,v;
@@ -38,4 +42,41 @@ void init()
 		edge[u].push_back(v);
 		edge[v].push_back(u);
 	}
+	build(1);
+	return;
 }
+
+void dp1(int root)
+{
+	int s0=0,s1=0;
+	for(auto i:tr[root].sons)
+	{
+		dp1(i);
+		s0+=tr[i].dps1;
+		s1+=min(tr[i].dps1,tr[i].dps0);
+	}
+	tr[root].dps0=s0;
+	tr[root].dps1=s1+tr[root].w;
+	return;
+}
+
+void dp2(int root,long long f0,long long f1)
+{
+	f0-=tr[root].dps1;
+	f1-=min(tr[root].dps0,tr[root].dps1);
+	tr[root].dpf0=f1;
+	tr[root].dpf1=f0+tr[root].w;
+	f0=tr[root].dpf0;
+	f1=tr[root].dpf1;
+	for(auto i:tr[root].sons)
+	{
+		f0+=tr[i].dps1;
+		f1+=min(tr[i].dps0,tr[i].dps1);
+	}
+	for(auto i:tr[root].sons)
+	{
+		dp2(i,f0,f1);
+	}
+	return;
+}
+
