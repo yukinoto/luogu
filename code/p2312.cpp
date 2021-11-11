374bacf503c18185cd582ed9ec0e92e2870e81f7
yukinoto <2733687567@qq.com>
2021-11-11 09:35:24 +0800

    .

--- code/p2312.cpp
+++ code/p2312.cpp
@@ -1,9 +1,58 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 
+long long *a;
 int n,m;
-int a[110];
 
-
-void check(int x)
+long long getn(istream &ins,long long mod=1000000007)
 {
+	char c=ins.get();
+	long long ans=0;
+	bool flag=false;
+	while(isspace(c))
+		c=ins.get();
+	if(c=='-')
+	{
+		flag=true;
+		c=ins.get();
+	}
+	while(isdigit(c))
+	{
+		ans=(ans*10+c-'0')%mod;
+		c=ins.get();
+	}
+	if(flag)
+		ans=mod-ans;
+	return ans;
+}
+
+bool check(long long x,long long mod=1000000007)
+{
+	long long p=1;
+	long long ans=0;
+	for(int i=0;i<=n;i++)
+	{
+		ans=(ans+p*a[i])%mod;
+		p=p*x%mod;
+	}
+	return ans==0;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin>>n>>m;
+	a=new long long[n+1];
+	for(int i=0;i<=n;i++)
+		a[i]=getn(cin);
+	int cnt=0,*as=new int[m];
+	for(int i=1;i<=m;i++)
+		if(check(i))
+			as[cnt++]=i;
+	cout<<cnt<<endl;
+	for(int i=0;i<cnt;i++)
+		cout<<as[i]<<endl;
+	delete[] a;
+	delete[] as;
+	return 0;
 }
--- code/p2827.cpp
+++ code/p2827.cpp
@@ -1,33 +1,141 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
 using namespace std;
+
+long long fln(queue<long long>&len,queue<long long>&len1,queue<long long>&len2)
+{
+	long long ans;
+	if(len.empty()&&len1.empty())
+	{
+		ans=len2.front();
+		len2.pop();
+		return ans;
+	}
+	if(len1.empty()&&len2.empty())
+	{
+		ans=len.front();
+		len.pop();
+		return ans;
+	}
+	if(len.empty()&&len2.empty())
+	{
+		ans=len1.front();
+		len1.pop();
+		return ans;
+	}
+	if(len.empty())
+	{
+		if(len1.front()>=len2.front())
+		{
+			ans=len1.front();
+			len1.pop();
+			return ans;
+		}
+		else
+		{
+			ans=len2.front();
+			len2.pop();
+			return ans;
+		}
+	}
+	if(len1.empty())
+	{
+		if(len.front()>=len2.front())
+		{
+			ans=len.front();
+			len.pop();
+			return ans;
+		}
+		else
+		{
+			ans=len2.front();
+			len2.pop();
+			return ans;
+		}
+	}
+	if(len2.empty())
+	{
+		if(len.front()>=len1.front())
+		{
+			ans=len.front();
+			len.pop();
+			return ans;
+		}
+		else
+		{
+			ans=len1.front();
+			len1.pop();
+			return ans;
+		}
+	}
+	if(len.front()>=len1.front())
+	{
+		if(len.front()>=len2.front())
+		{
+			ans=len.front();
+			len.pop();
+			return ans;
+		}
+		else
+		{
+			ans=len2.front();
+			len2.pop();
+			return ans;
+		}
+	}
+	else
+	{
+		if(len1.front()>=len2.front())
+		{
+			ans=len1.front();
+			len1.pop();
+			return ans;
+		}
+		else
+		{
+			ans=len2.front();
+			len2.pop();
+			return ans;
+		}
+	}
+}
+
+template<typename Int>
+Int greate(Int x,Int y)
+{
+	return x>y;
+}
 
 int main()
 {
 	int n,m;
 	int q,t;long long u,v;
 	cin>>n>>m>>q>>u>>v>>t;
-	priority_queue<long long> len;
+	//priority_queue<long long> len;
+	queue<long long>len,len1,len2;
+	long long *a=new long long[n];
 	for(int i=0;i<n;i++)
 	{
-		long long a;
-		cin>>a;
-		len.push(a);
+		cin>>a[i];
 	}
+	sort(a,a+n,greate<long long>);
+	for(int i=0;i<n;i++)
+		len.push(a[i]);
 	for(int i=1;i<=m;i++)
 	{
-		long long x=len.top();len.pop();
+		long long x=fln(len,len1,len2);
 		x+=q*(i-1);
-		len.push(x*u/v-q*i),len.push(x-x*u/v-q*i);
+		len1.push(x*u/v-q*i),len2.push(x-x*u/v-q*i);
 		if(i%t==0)
 			cout<<x<<' ';
 	}
 	cout<<endl;
-	for(int i=1;!len.empty();i++)
+	for(int i=1;i<=n+m;i++)
 	{
+		long long ans=fln(len,len1,len2);
 		if(i%t==0)
-			cout<<len.top()+q*m<<' ';
-		len.pop();
+			cout<<ans+q*m<<' ';
 	}
 	cout<<endl;
 	return 0;
