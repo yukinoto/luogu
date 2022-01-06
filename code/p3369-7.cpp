9c9d8f96f3c58365e853ee6e94731adcff417e74
yukinoto <2733687567@qq.com>
2022-01-06 10:40:23 +0800

    .

--- code/p3369-7.cpp
+++ code/p3369-7.cpp
@@ -1,5 +1,9 @@
 #define DEBUG
 
+#define LEFT true
+#define RIGHT false
+typedef int size_t;
+
 template<typename T>
 class Splay{
 	private:
@@ -10,14 +14,16 @@ class Splay{
 				size_t size,cnt;
 				node(){;}
 				~node(){delete lc,delete rc;}
-				node(const T&value,node *fa):this->value(value),this->fa(fa){;}
+				node(const T&value,node *fa){this->value=value,this->fa=fa;}
 		};
 		node *root;
-		void rotate(node *);
+		void rotate(node *,bool);
 		void splay(node *);
 		void pushup(node *);
 		void erase(node *);
 	public:
+		Splay():root(nullptr){;}
+		~Splay(){delete root;}
 		class iterator;
 		iterator insert(const T&);
 		void erase(const iterator&);
@@ -31,8 +37,8 @@ class Splay<T>::iterator{
 		iterator& operator ++();
 		iterator& operator --();
 		const T& operator*();
-		friend iterator insert(const T&);
-		friend iterator erase(node *);
+		friend Splay<T>::iterator Splay<T>::insert(const T&);
+		friend Splay<T>::iterator Splay<T>::erase(node *);
 };
 
 template<typename T>
@@ -67,3 +73,65 @@ typename Splay<T>::iterator& Splay<T>::iterator::operator--()
 	return *this;
 }
 
+template<typename T>
+const T& Splay<T>::iterator::operator*()
+{
+	return *pt;
+}
+
+template<typename T>
+void Splay<T>::rotate(node *pt,bool dir)
+{
+	if (dir==LEFT)
+	{
+		node *l=pt->lc;
+		l->fa=pt->fa;
+		pt->lc=l->rc;
+		l->rc=pt;
+		pt->fa=l;
+		if(pt->lc!=nullptr)
+			pt->lc->fa=pt;
+		if(l->fa!=nullptr)
+		{
+			if(l->fa->lc==pt)
+				l->fa->lc=l;
+			else
+				l->fa->rc=l;
+		}
+	}
+	else
+	{
+		node *r=pt->rc;
+		r->fa=pt->fa;
+		pt->rc=r->lc;
+		r->lc=pt;
+		pt->fa=r;
+		if(pt->rc!=nullptr)
+			pt-rc->fa=pt;
+		if(r->fa!=nullptr)
+		{
+			if(r->fa->lc==pt)
+				r->fa->lc=r;
+			else
+				r->fa->rc=r;
+		}
+	}
+	return;
+}
+
+template<typename T>
+void Splay<T>::splay(node *pt)
+{
+	auto f=[this](node *pt)->void{if(pt->fa->lc==pt)rotate(pt->fa,LEFT)else rotate(pt->fa,RIGHT);};
+	auto dt=[](node *pt)->bool{if(pt->fa->lc==pt)return LEFT;else return RIGHT;};
+	while(pt->fa!=nullptr)
+	{
+		if(pt->fa->fa==nullptr||dt(pt)!=dt(pt->fa))
+			f(pt);
+		else
+			f(pt->fa);
+	}
+	return;
+}
+
+#undef DEBUG
